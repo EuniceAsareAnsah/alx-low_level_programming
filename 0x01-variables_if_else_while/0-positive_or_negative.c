583e04948d75d778e769243eb1035777e316414b
EuniceAsareAnsah <euniceansah98@gmail.com>
2023-02-18 05:40:29 -0800

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <studio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - Entry point
+*Description: 'outputsa a sentence with puts'
+*Return: Always 0(Sucess)
+*/
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+else
+{
+printf("%d is zero\n", n);
+}
+return (0);
+}
