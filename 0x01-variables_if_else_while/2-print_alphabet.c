ae7c52b6375f76206bb80f0ff50252c9b8ff22ab
EuniceAsareAnsah <euniceansah98@gmail.com>
2023-02-18 07:27:56 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
 *main - Entry point
-Description: 'outputs a sentence with puts'
+*Description: 'outputs a sentence with puts'
 *Return: Always 0(Success)
 */
 int main(void)
