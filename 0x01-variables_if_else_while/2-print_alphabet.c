174cc065e893189eed73162bfa6fd7d6c8dc8d2a
EuniceAsareAnsah <euniceansah98@gmail.com>
2023-02-18 07:22:54 -0800

    2-print-alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - Entry point
+Description: 'outputs a sentence with puts'
+*Return: Always 0(Success)
+*/
+int main(void)
+{
+char n = 'a';
+while (n <= 'z')
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
+}
