df7a759d37ee885bc82550864e6e2fe16ff54edb
EuniceAsareAnsah <euniceansah98@gmail.com>
2023-02-19 13:57:57 -0800

    101 quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+/**
+*main - A c program that print a line to a standard error
+*Return: 0 (Success)
+*/
+int main(void)
+{
+system("echo 'and that piece of art is useful\"- Dora Korpar, 2015-10-19");
+return (1);
+}
