583968a92ee030cf46f02232f0c5132a1d5e0be8
EuniceAsareAnsah <euniceansah98@gmail.com>
2023-02-19 12:16:20 -0800

    print

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,13 +8,14 @@
 int main(void)
 {
 int d;
-for (d = '0'; d < '9'; d++)
+for (d = '0'; d <= '9'; d++)
 {
 putchar(d);
 if (d != '9')
 {
 putchar(',');
 putchar(' ');
+}
 }
 putchar('\n');
 return (0);
