a4912ef3d099ac92aedd20158c6b115f5b0a67e3
EuniceAsareAnsah <euniceansah98@gmail.com>
2023-02-22 11:07:50 -0800

    2printalphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - prints 10x the alphabets
+*description: 'prints alphabets'
+*Return: void
+*/
+void print_alphaber_x10(void)
+{
+int i;
+char a = 'a';
+for (i = 0 ; i < 10 ; i++)
+{
+for (a = 'a' ; a <= 'z' ; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
+}
