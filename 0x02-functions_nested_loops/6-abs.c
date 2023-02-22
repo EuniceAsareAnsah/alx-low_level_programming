54ec4285c4faceb767f431814827d77f2308cbef
EuniceAsareAnsah <euniceansah98@gmail.com>
2023-02-22 13:25:43 -0800

    6abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*_abs - returns the absolute of the input
+*@a: it is an integer
+*Description: returns the absolute of the input'
+*Return: 0
+*/
+int _abs(int a)
+{
+if (a >= 0)
+{
+return (a);
+}
+return (-a);
+}
