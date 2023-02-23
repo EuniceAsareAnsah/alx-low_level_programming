ac0977c7a511c530a4b9259f042943b57ea27482
EuniceAsareAnsah <euniceansah98@gmail.com>
2023-02-22 18:28:22 -0800

    10add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*add - a function
+*@c: an integer
+*@d: an integer
+*Description: prints every minute of the days ofJack Bauer
+*Return: Always 0
+*/
+int add(int c, int d)
+{
+return (c + d);
+}
