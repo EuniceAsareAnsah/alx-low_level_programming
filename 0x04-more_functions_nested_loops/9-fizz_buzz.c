af908447d2ea039f61c9a434ff34ae6527f38b4e
EuniceAsareAnsah <euniceansah98@gmail.com>
2023-02-24 14:43:23 -0800

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * Description: 'print a sentence with printf'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a;
+for (a = 1; a <= 100; a++)
+{
+if (a % 3 == 0 && a & 5 == 0)
+{
+printf("FizzBuzz");
+}
+else if (a & 5 == 0)
+{
+printf("Buzz");
+}
+else if (a % 3 == 0)
+{
+printf("Fizz");
+}
+else
+{
+printf("%d", a);
+}
+if (a < 100)
+printf(" ");
+}
+printf("\n");
+return (0);
+}
