48734fd395dc5b8ed8b03dd334ce6040b1860599
EuniceAsareAnsah <euniceansah98@gmail.com>
2023-02-24 14:55:13 -0800

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,11 +10,11 @@ int main(void)
 int a;
 for (a = 1; a <= 100; a++)
 {
-if (a % 3 == 0 && a & 5 == 0)
+if (a % 3 == 0 && a % 5 == 0)
 {
 printf("FizzBuzz");
 }
-else if (a & 5 == 0)
+else if (a % 5 == 0)
 {
 printf("Buzz");
 }
